1eaf393406dbacdf41e78241e0158ca7e68a2fb2
Simo Aleksandrov <simo3003@me.com>
2022-07-29 22:36:06 +0300

    feat: make more user friendly

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
 	"github.com/emicklei/dot"
@@ -23,34 +25,66 @@ type User struct {
 	PublicFlags   int64  `json:"public_flags"`
 }
 
+type UserID string
+
 func main() {
-	graphFile, err := os.Open("./graph.json")
+	var token string
+	fmt.Println("enter your token:")
+	fmt.Scanln(&token)
+	m, v, err := fetchRelationships(token)
 	if err != nil {
 		panic(err)
 	}
+	g := buildGraph(m, v)
+	if err := os.WriteFile("relationships.dot", []byte(g.String()), 0644); err != nil {
+		panic(err)
+	}
+}
 
-	relationshipsFile, err := os.Open("./relationships.json")
+func fetchRelationships(token string) (map[UserID][]UserID, []Relationship, error) {
+	client := &http.Client{}
+	log.Println("fetching @me relationships")
+	res, err := fetchDiscordEndpoint(*client, token, "GET", "https://discordapp.com/api/v6/users/@me/relationships")
 	if err != nil {
-		panic(err)
+		return nil, nil, err
 	}
 
-	m := map[string][]string{}
-	if err := json.NewDecoder(graphFile).Decode(&m); err != nil {
-		panic(err)
+	defer res.Body.Close()
+	myRelationships := []Relationship{}
+	if err := json.NewDecoder(res.Body).Decode(&myRelationships); err != nil {
+		return nil, nil, err
 	}
 
-	r := []Relationship{}
-	if err := json.NewDecoder(relationshipsFile).Decode(&r); err != nil {
-		panic(err)
+	graph := map[UserID][]UserID{}
+	for _, relationship := range myRelationships {
+		log.Println("fetching relationship", relationship.ID)
+		res, err := fetchDiscordEndpoint(*client, token, "GET", fmt.Sprintf("https://discordapp.com/api/v6/users/%s/relationships", relationship.ID))
+		if err != nil {
+			return nil, nil, err
+		}
+
+		theirRelationships := []Relationship{}
+		if err := json.NewDecoder(res.Body).Decode(&theirRelationships); err != nil {
+			return nil, nil, err
+		}
+
+		for _, theirRelationship := range theirRelationships {
+			graph[UserID(relationship.ID)] = append(graph[UserID(relationship.ID)], UserID(theirRelationship.ID))
+		}
+		res.Body.Close()
 	}
 
-	relationships := map[string]User{}
-	for _, v := range r {
+	return graph, myRelationships, nil
+}
+
+func buildGraph(m map[UserID][]UserID, myRelationships []Relationship) *dot.Graph {
+	relationships := map[UserID]User{}
+	for _, v := range myRelationships {
 		if v.Type != 1 {
 			continue
 		}
 
-		relationships[v.ID] = v.User
+		relationships[UserID(v.ID)] = v.User
 	}
 
 	g := dot.NewGraph(dot.Directed)
@@ -64,13 +98,22 @@ func main() {
 			continue
 		}
 
-		fromNode := g.Node(k)
+		fromNode := g.Node(string(k))
 		fromNode.Label(relationships[k].Username)
 		for _, to := range v {
-			toNode := g.Node(to)
+			toNode := g.Node(string(to))
 			toNode.Label(relationships[to].Username)
-			g.Edge(fromNode, toNode, )
+			g.Edge(fromNode, toNode)
 		}
 	}
-	fmt.Println(g.String())
+	return g
+}
+
+func fetchDiscordEndpoint(client http.Client, token, method, endpoint string) (*http.Response, error) {
+	req, err := http.NewRequest(method, endpoint, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("authorization", token)
+	return client.Do(req)
 }
